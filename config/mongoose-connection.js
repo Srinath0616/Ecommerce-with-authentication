4862984b56bd1125b3c5f2833b8a9f7c45111e66
srinathgoud1 <srinathgoud272@gmail.com>
2026-07-13 13:55:02 +0530

    Removed Config

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const config = require("config");
 
 const dbgr = require("debug")("development:mongoose");
 // Here we've saved these environment variable(development:mongoose) in the memory itself so no one can ever acess or read these even if they get access to your personal computer
