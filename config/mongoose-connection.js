9f3b41fbbae4c090d8f77d4c99e98f785c0e113c
srinathgoud1 <srinathgoud272@gmail.com>
2026-07-13 13:40:17 +0530

    Prepare for Railway deployment

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -5,12 +5,12 @@ const dbgr = require("debug")("development:mongoose");
 // Here we've saved these environment variable(development:mongoose) in the memory itself so no one can ever acess or read these even if they get access to your personal computer
 
 mongoose
-// .connect(`%{}`)
-.connect(`${config.get("MONGODB_URI")}/Ecomm`)
-.then(function(){
+  .connect(process.env.MONGODB_URI)
+  .then(function () {
     dbgr("Connected to db");
-}).catch(function(err){
+  })
+  .catch(function (err) {
     dbgr(err);
-});
+  });
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -5,26 +5,25 @@ const ownerModel = require("../models/owner-model");
 // console.log(process.env.NODE_ENV)
 // Here we've saved these environment variable in the memory itself so no one can ever acess or read these even if they get access to your personal computer, So this works only till the development
 
-if (process.env.NODE_ENV === "development") {
-  router.post("/create", async function (req, res) {
-    let owners = await ownerModel.find();
-    if (owners.length > 0) {
-      return res.status(404).send("Unauthorized access");
-    }
-    let {fullname,email,password,gstin} = req.body;
-    let createdOwner = await ownerModel.create({
-      fullname,
-      email,
-      password,
-      gstin
-    });
-    res.status(201).send(createdOwner);
+router.post("/create", async function (req, res) {
+  let owners = await ownerModel.find();
+  if (owners.length > 0) {
+    return res.status(404).send("Unauthorized access");
+  }
+  let { fullname, email, password, gstin } = req.body;
+  let createdOwner = await ownerModel.create({
+    fullname,
+    email,
+    password,
+    gstin,
   });
-} // When environment variable(NODE_ENV ) is set to development then only it'll work else it won't work if it is production or even if it is set to nothing
+  res.status(201).send(createdOwner);
+});
+// When environment variable(NODE_ENV ) is set to development then only it'll work else it won't work if it is production or even if it is set to nothing
 
 router.get("/admin", function (req, res) {
-  let success = req.flash("success")
-  res.render("createproducts",{success});
+  let success = req.flash("success");
+  res.render("createproducts", { success });
 });
 
 module.exports = router;
