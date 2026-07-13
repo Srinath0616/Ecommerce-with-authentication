df1df41b793b8856e6727733765f70b8309d4d0c
srinathgoud1 <srinathgoud272@gmail.com>
2026-07-13 14:21:20 +0530

    Mongo db updates

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const dbgr = require("debug")("development:mongoose");
 // Here we've saved these environment variable(development:mongoose) in the memory itself so no one can ever acess or read these even if they get access to your personal computer
-
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(function () {
-    dbgr("Connected to db");
+  .then(() => {
+    console.log("✅ MongoDB Connected");
   })
-  .catch(function (err) {
-    dbgr(err);
+  .catch((err) => {
+    console.error("❌ MongoDB Error:", err);
   });
-
 module.exports = mongoose.connection;
