ce2b9455a223fb5a626949e1ac9947bade493776
srinathgoud1 <srinathgoud272@gmail.com>
2026-07-27 22:12:22 +0530

    mongodb connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,8 +4,8 @@ const dbgr = require("debug")("development:mongoose");
 // Here we've saved these environment variable(development:mongoose) in the memory itself so no one can ever acess or read these even if they get access to your personal computer
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Ecomm") 
-  // .connect(process.env.PORT) // Add process.env.PORT Here when pushing to git and for development purpose use mongodb://127.0.0.1:27017/Ecomm
+  // .connect("mongodb://127.0.0.1:27017/Ecomm") 
+  .connect(process.env.PORT) // Add process.env.PORT Here when pushing to git and for development purpose use mongodb://127.0.0.1:27017/Ecomm
   .then(() => {
     console.log("✅ MongoDB Connected");
   })
