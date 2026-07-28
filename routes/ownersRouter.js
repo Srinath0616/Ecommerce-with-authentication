aa34a1cd0b69adc7a73f35004aae2accf6a4fe1f
srinathgoud1 <srinathgoud272@gmail.com>
2026-07-28 19:34:25 +0530

    refactor: rename EJS view files

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ownerModel = require("../models/owner-model");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
 // console.log(process.env.NODE_ENV)
 // Here we've saved these environment variable in the memory itself so no one can ever acess or read these even if they get access to your personal computer, So this works only till the development
 
-router.post("/create", async function (req, res) {
+router.post("/create",isLoggedIn, async function (req, res) {
   let owners = await ownerModel.find();
   if (owners.length > 0) {
     return res.status(404).send("Unauthorized access");
@@ -21,7 +22,7 @@ router.post("/create", async function (req, res) {
 });
 // When environment variable(NODE_ENV ) is set to development then only it'll work else it won't work if it is production or even if it is set to nothing
 
-router.get("/admin", function (req, res) {
+router.get("/admin",isLoggedIn, function (req, res) {
   let success = req.flash("success");
   res.render("createproducts", { success });
 });
